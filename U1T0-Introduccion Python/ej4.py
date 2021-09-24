8bb2bf32f0980957df67d4ff6cb7b8fd328ef0c6
Ángela Bonilla <44753691+angelaagmez@users.noreply.github.com>
2021-09-24 16:15:03 +0200

    Add files via upload

---
+++ U1T0-Introduccion Python/ej4.py
@@ -0,0 +1,12 @@
+# Convierta un número decimal en octal usando print() con formato de salida.
+
+# Dado: num = 8
+
+# Resultado: El número octal del número decimal 8 es 10
+
+dec = 8
+
+print("La conversion de",dec,"a octal es: ", oct(dec))
+
+
+print("%o"%dec)
