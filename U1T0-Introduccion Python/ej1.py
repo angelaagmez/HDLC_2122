3e74a9670f4477df1609d43358d1a6c3dc5a4134
Ángela Bonilla <44753691+angelaagmez@users.noreply.github.com>
2021-09-30 20:43:48 +0200

    Delete U1T0-Introduccion Python directory

--- U1T0-Introduccion Python/ej1.py
+++
@@ -1,6 +0,0 @@
-# Escriba un programa para aceptar dos números del usuario y calcular la multiplicación
-num1 = int(input("Introduzca un numero: "))
-num2 = int(input("Introduzca otro numero: "))
-
-
-print(num1,"x",num2,"=",num1 * num2)
